2176f7786bdd46640b4022f9e718a119f1c65b33
Adam <adam@adamr.co.uk>
2024-12-04 07:31:36 +0000

    start of day4

---
+++ 04.py
@@ -0,0 +1,13 @@
+from copy import deepcopy
+with open('./04.test') as file:
+    data = [line.strip() for line in file]
+word = 'XMAS'
+
+def checkForWord(i,)
+
+xs = [-1, 0, 1]
+ys = [-1, 0, 1]
+for xx, row in enumerate(data):
+    for yy, c in enumerate(row):
+        if c == 'X':
+            
