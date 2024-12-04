0ad825e76d643f0bec5b4371276023949f41c043
Adam R <adam@adamr.co.uk>
2024-12-04 12:15:25 +0000

    4 - getting there

--- 04.py
+++ 04.py
@@ -7,22 +7,32 @@ xs = [-1, 0, 1]
 ys = [-1, 0, 1]
 
 
-def checkletters(xx, yy, currstring, wordindex, t):
-    if currstring == word:
+def checknext(xx, yy, xdir, ydir, currentstring, index, found):
+    if index > len(word) - 1:
+        found = True
+    if found:
         return 1
+    if xx + xdir > 0 and xx + xdir < len(data[0]):
+        if yy + ydir > 0 and yy + ydir < len(data):
+            if data[xx + xdir][yy + ydir] == word[index]:
+                currentstring += data[xx + xdir][yy + ydir]
+                t = checknext(xx + xdir, yy + ydir, xdir, ydir,
+                              currentstring, index + 1, found)
+                if t == 1:
+                    return 1
+    return 0
+
+
+def checkletters(xx, yy, currstring, wordindex, t):
     for x1 in xs:
         if xx + x1 < 0 or xx + x1 > len(data[0])-1:
             continue
         for y1 in ys:
             if yy + y1 < 0 or yy + y1 > len(data)-1:
                 continue
-            if data[xx + x][yy + y] == word[wordindex]:
-                currstring += data[xx + x1][yy + y1]
-                t += checkletters(xx + x1, yy + y1,
-                                  currstring, wordindex + 1, t)
             else:
-                continue
-    return 0
+                t += checknext(xx, yy, x1, y1, currstring, wordindex, False)
+    return t
 
 
 for x, row in enumerate(data):
