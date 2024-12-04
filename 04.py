cf94ade2755c6eafc56789ebe54dc7e80cbc9252
Adam R <adam@adamr.co.uk>
2024-12-04 11:12:33 +0000

    4 - struggling

--- 04.py
+++ 04.py
@@ -1,13 +1,34 @@
-from copy import deepcopy
 with open('./04.test') as file:
     data = [line.strip() for line in file]
 word = 'XMAS'
-
-def checkForWord(i,)
+total = 0
 
 xs = [-1, 0, 1]
 ys = [-1, 0, 1]
-for xx, row in enumerate(data):
-    for yy, c in enumerate(row):
-        if c == 'X':
-            
+
+
+def checkletters(xx, yy, currstring, wordindex, t):
+    if currstring == word:
+        return 1
+    for x1 in xs:
+        if xx + x1 < 0 or xx + x1 > len(data[0])-1:
+            continue
+        for y1 in ys:
+            if yy + y1 < 0 or yy + y1 > len(data)-1:
+                continue
+            if data[xx + x][yy + y] == word[wordindex]:
+                currstring += data[xx + x1][yy + y1]
+                t += checkletters(xx + x1, yy + y1,
+                                  currstring, wordindex + 1, t)
+            else:
+                continue
+    return 0
+
+
+for x, row in enumerate(data):
+    for y, c in enumerate(row):
+        if c == word[0]:
+            currentword = word[0]
+            total += checkletters(x, y, currentword, 1, 0)
+
+print(total)
