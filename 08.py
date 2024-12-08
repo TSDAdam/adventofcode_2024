3d1e2826f3c02cc32245aadb5564b37e6d87201f
Adam <adam@adamr.co.uk>
2024-12-08 16:15:31 +0000

    8 not quite there

--- 08.py
+++ 08.py
@@ -1,6 +1,6 @@
 from copy import deepcopy
 from collections import defaultdict
-with open('./08.in') as file:
+with open('./08.test') as file:
     data = [line.strip() for line in file]
 
 R = len(data[0])
@@ -8,7 +8,7 @@ C = len(data)
 antenodes = set()
 p2antenodes = set()
 antennatypes = defaultdict(list)
-for y, row in enumerate(data):
+for y, row in enumerate(data): # parse input
     for x, c in enumerate(row):
         if c != '.':
             antennatypes[c].append((int(y), int(x))) 
@@ -19,12 +19,12 @@ for k, v in antennatypes.items():
         for l in range(i+1, len(v)):
             diffy = abs(loc[0] - v[l][0])
             diffx = abs(loc[1] - v[l][1])
-            if loc[0] < v[l][0]:
-                y1 = loc[0] - diffy
-                y2 = v[l][0] + diffy
+            if loc[0] < v[l][0]: # if this node is smaller Y
+                y1 = loc[0] - diffy # make the antenode have minus the difference
+                y2 = v[l][0] + diffy # and the opposite have positive
                 stepy1 = loc[0] - diffy
                 stepy2 = v[l][0] + diffy
-            elif loc[0] > v[l][0]:
+            elif loc[0] > v[l][0]: # as above, but the opposite way around
                 y1 = loc[0] + diffy
                 y2 = v[l][0] - diffy
                 stepy1 = loc[0] + diffy
@@ -35,7 +35,7 @@ for k, v in antennatypes.items():
                 stepy1 = 0
                 stepy2 = 0
             if loc[1] < v[l][1]:
-                x1 = loc[1] - diffx
+                x1 = loc[1] - diffx # same as the Y co-ords, but X
                 x2 = v[l][1] + diffx
                 stepx1 = loc[1] - diffx
                 stepx2 = v[l][1] + diffx
@@ -49,7 +49,8 @@ for k, v in antennatypes.items():
                 x2 = loc[1]
                 stepx1 = 0
                 stepx2 = 0
-            if 0 <= y1 < R and 0 <= x1 < C:
+
+            if 0 <= y1 < R and 0 <= x1 < C: # if antenode isn't OOB
                 antenodes.add((y1, x1))
                 while 0 <= y1 < R and 0 <= x1 < C:
                     p2antenodes.add((y1, x1))
@@ -57,6 +58,7 @@ for k, v in antennatypes.items():
                     x1 += stepx1
             if 0 <= y2 < R and 0 <= x2 < C:
                 antenodes.add((y2, x2))
+                p2antenodes.add((y2, x2))
                 while 0 <= y2 < R and 0 <= x2 < C:
                     p2antenodes.add((y2, x2))
                     y2 += stepy2
