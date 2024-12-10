c8df9e91ba6b7420c9ff12082762b29195375ce0
Adam <adam@adamr.co.uk>
2024-12-10 13:06:16 +0000

    10 continued

--- 10.py
+++ 10.py
@@ -14,7 +14,7 @@ C = len(data)
 validpaths = []
 dirs = [(-1, 0), (0, +1), (+1, 0), (0, -1)]
 
-def tracepath(r, c, height, score):
+def tracepath(r, c, height, score, found9s):
     for dir in dirs:
         rx = r + dir[0]
         cx = c + dir[1]
@@ -22,15 +22,21 @@ def tracepath(r, c, height, score):
             print(data[rx][cx])
             if data[rx][cx] == height + 1:
                 if data[rx][cx] == 9:
-                    return score + 1
+                    if (rx, cx) not in found9s:
+                        found9s.append((rx, cx))
+                        return score + 1
+                    else:
+                        return score
                 else:
-                    score += tracepath(rx, cx, height + 1, score)
+                    score += tracepath(rx, cx, height + 1, 0, found9s)
 
     return score
 
 
 for r ,row in enumerate(data):
     for c, col in enumerate(row):
+        found9 = []
         if col == 0:
-            validpaths.append(tracepath(r, c, col, 0))
+            validpaths.append(tracepath(r, c, col, 0, found9))
 print(validpaths)
+print(sum( x for x in validpaths))
